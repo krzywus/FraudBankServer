5e30a23cbf249d6d7f3e18e51ac818e79dbec517
krzywus <filip.krzywy@gmail.com>
2017-11-18 13:44:07 +0100

    spring server init

---
+++ src/main/java/com/krzywus/bkl4/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.krzywus.bkl4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
